ee42d3e3c4f605a6c15b41f38f817251b055c6b0
Arithmetic-Overflow <aryageza@gmail.com>
2021-05-11 16:10:24 +0800

    Added moving average by bounds

--- moving_averages.py
+++ moving_averages.py
@@ -33,3 +33,62 @@ def moving_average(prefix_sum, num_timesteps, start_timestep):
 	print(f"Sum from index {start_timestep:2.0f} to {end_timestep:2.0f} : {range_mean:.2f}")
 
 	return range_mean
+
+def moving_average_by_bounds(prefix_sum, lower, upper):
+	if lower < 0:
+		print("f")
+		return
+
+	if upper >= len(prefix_sum):
+		print("f")
+		return
+
+	if lower != 0:
+		lower_pref = prefix_sum[lower - 1]
+		# len_range = upper - lower + 1
+
+	else:
+		lower_pref = 0
+		# len_range = upper - lower
+
+	# print("l", lower_pref, )
+
+	upper_pref = prefix_sum[upper]
+
+
+	ans = (upper_pref - lower_pref) / (upper - lower + 1)
+	print(lower_pref, upper_pref, ans)
+	return ans
+
+print("Test cases:")
+
+prices = [1,2,3,4,5,6,7,8]
+prefsum = prefix_sum_init(prices)
+print(prefsum)
+print(prices)
+print()
+
+prices_append(9, prices, prefsum)
+moving_average_by_bounds(prefsum, 3, 5)
+
+print()
+print(prefsum)
+print(prices)
+print()
+
+prices_append(10, prices, prefsum)
+moving_average_by_bounds(prefsum, 5, 8)
+moving_average_by_bounds(prefsum, 5, 9)
+
+print()
+print(prefsum)
+print(prices)
+print()
+
+prices_append(10, prices, prefsum)
+moving_average_by_bounds(prefsum, 5, 9)
+
+print()
+print(prefsum)
+print(prices)
+print()
