6d1ffdb2829bea8609b7b1d2062ca5033531aa5e
Arithmetic-Overflow <aryageza@gmail.com>
2021-06-14 14:22:55 +0800

    made classes to inherit from

--- trading_sim.py
+++ trading_sim.py
@@ -6,28 +6,7 @@ import matplotlib.pyplot as plt
 
 # num_mins = 24180
 
-class FakeBot:
-    def __init__(self, stocks):
-        self.Initialize(stocks)
-
-    def Initialize(self, stocks):
-        self.past_intervals = 30
-        self.time           = self.past_intervals
-        self.end_time       = min([len(i.pricedata) - self.past_intervals for i in stocks])
-
-        self.z_threshold    = 2.0
-
-        self.stocks         = stocks
-        self.prices         = [s.pricedata for s in self.stocks]
-        
-        self.Invested       = False
-        self.bank           = 100000
-        self.stocksOwned    = [0 for i in self.stocks]
-
-        # self.output = {}
-
-        self.output_init()
-
+class BotBase:
     def Sell(self, stockNumber, volume=1):
         self.bank                       += volume * self.prices[stockNumber][self.time]
         self.stocksOwned[stockNumber]   -= volume
@@ -43,8 +22,45 @@ class FakeBot:
         self.stocksOwned[stockNumber] = 0
 
         netchange = volume * price
-
         self.bank += netchange
+        
+    def output_init(self):
+        self.output                 = {}
+
+        time = self.time
+
+        self.output['finished']     = False
+        self.output['print']        = []
+        self.output['time']         = self.time
+
+
+class FakeBot(BotBase):
+    def __init__(self, stocks):
+        self.Initialize(stocks)
+
+    def Initialize(self, stocks):
+        self.past_intervals = 12
+        self.time           = self.past_intervals
+        self.end_time       = min([len(i.pricedata) - self.past_intervals for i in stocks])
+
+        self.z_threshold    = 2.0
+
+        self.stocks         = stocks
+        self.prices         = [s.pricedata for s in self.stocks]
+        
+        self.Invested       = False
+        self.bank           = 10
+        self.stocksOwned    = [0 for i in self.stocks]
+
+        # self.output = {}
+
+        self.output_init()
+
+    def Debug(self, obj):
+        self.output['print'] = self.output.get('print', []) + [obj]
+
+    def worth(self):
+        return self.bank + sum([i*j[self.time] for i, j in zip(self.stocksOwned, self.prices)])
 
     def OnData(self):
         self.time += 1
@@ -68,14 +84,15 @@ class FakeBot:
                 self.Invested = True
                 # Need to determine buy ratio
                 if z_score > 0.0:
+                    trading = f'BUY {self.stocks[1]}, SELL {self.stocks[0]}'
                     self.Sell(0,1)
                     self.Buy(1, 1)
-                    trading = f'BUY {self.stocks[1]}, SELL {self.stocks[0]}'
+
                 else:
                     trading = f'SELL {self.stocks[1]}, BUY {self.stocks[0]}'
                     self.Sell(1,1)
                     self.Buy(0, 1)
-        else:
+        elif abs(z_score) < self.z_threshold / 2:
             if self.Invested:
                 trading = "LIQUIDATE"
                 self.Invested = False
@@ -96,18 +113,6 @@ class FakeBot:
         
         return self.output
             
-    def output_init(self):
-        self.output                 = {}
-
-        time = self.time
-
-        self.output['finished']     = False
-        self.output['print']        = []
-        self.output['time']         = self.time
-
-    def Debug(self, obj):
-        self.output['print'] = self.output.get('print', []) + [obj]
-
 
     def history(self, data_list):
         return data_list[self.time - self.past_intervals: self.time]
@@ -123,6 +128,7 @@ class FakeBot:
         
         z = (diff[-1]-mean)/SD 
         return z
+
         
 
 def get_subset_stocks():
@@ -143,7 +149,12 @@ def test(manual=True):
     bot = FakeBot(stocks)
     halt = False
     print("ah")
+
     time_ints = np.arange(bot.end_time)
+    initial_bank = bot.bank
+    print(initial_bank)
+    bank      = np.zeros(bot.end_time + 1)
+
     print("oh")
     for stock in stocks:
         print("wow")
@@ -178,6 +189,7 @@ def test(manual=True):
             currtime = int(output['time'])
 
             print(f'{currtime}:', end="\r")
+            bank[currtime] = bot.worth()
 
             if output['finished']:
                 halt = True
@@ -191,11 +203,15 @@ def test(manual=True):
                     buypoints.append(currtime)
                     buyprices.append(p1)
 
+                    # print('buy')
+
                 else:
                     sellpoints.append(currtime)
                     sellprices.append(p1)
 
-            continue
+                    # print('buy')
+
+            # continue
 
             if output['print'] is None or output['print'] == []:
                 print('\tNothing to print')
@@ -204,11 +220,17 @@ def test(manual=True):
                 name = ''# obj.__name__
                 print(f'\t{name} :: {obj}')
 
-    plt.scatter(buypoints, buyprices, color = "red", marker = "x")
-    plt.scatter(sellpoints, sellprices, color = "green", marker = "x")
     print("bot terminated")
+
+    plt.scatter(buypoints, buyprices, color = "red", marker = "x")
+    plt.scatter(sellpoints, sellprices, color = "green", marker = ".")
+
+    plt.plot(100*(bank - initial_bank)/initial_bank)
+
     plt.savefig("plot_data.png")
     print("figure saved")
+
+    # print(stocks[0].pricedata)
 
 if __name__ == '__main__':
     test(manual=True)
